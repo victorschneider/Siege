bd30c4e65190b5ac70f194c82cf1aae1432e9cfc
augusto-rich <69879585+augusto-rich@users.noreply.github.com>
2022-07-18 15:36:06 -0300

    Add files via upload
    Banco de dados do sistema SIEGE.

---
+++ database_SIEGE.sql
@@ -0,0 +1,43 @@
+CREATE SCHEMA sns;
+CREATE TABLE sns.user (
+  id int NOT NULL,
+  firstName VARCHAR(50) NULL DEFAULT NULL,
+  lastName VARCHAR(50) NULL DEFAULT NULL,
+  username VARCHAR(50) NULL DEFAULT NULL,
+  email VARCHAR(50) NULL,
+  passwordHash VARCHAR(32) NOT NULL,
+  PRIMARY KEY (id));
+
+  CREATE TABLE sns.user_follower (
+  id BIGINT NOT NULL,
+  sourceId BIGINT NOT NULL,
+  targetId BIGINT NOT NULL,
+  type SMALLINT NOT NULL DEFAULT 0,
+  createdAt TIMESTAMP NOT NULL,
+  updatedAt TIMESTAMP NULL DEFAULT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_ufollower_source
+    FOREIGN KEY (sourceId)
+    REFERENCES sns.user (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+ALTER TABLE sns.user_follower 
+ADD CONSTRAINT fk_ufollower_target
+  FOREIGN KEY (targetId)
+  REFERENCES sns.user (id)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+  CREATE TABLE sns.user_post (
+  id BIGINT NOT NULL,
+  userId BIGINT NOT NULL,
+  post_bio VARCHAR NULL DEFAULT NULL,
+  createdAt TIMESTAMP NOT NULL,
+  updatedAt TIMESTAMP NULL DEFAULT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_upost_user
+    FOREIGN KEY (userId)
+    REFERENCES sns.user (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
